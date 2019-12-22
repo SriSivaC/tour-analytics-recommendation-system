c5ee2d8d66810727d084e3bdae62c23e791efa47
liho98 <tanlh-wa16@student.tarc.edu.my>
2019-12-22 18:47:11 +0000

    add tripadvisor attraction crawler

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -19,10 +19,10 @@ KAFKA_HOSTS = ["localhost:9092"]
 KAFKA_TOPICS = ["theculturetrip", "tripadvisor_attraction"]
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'crawler (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -67,9 +67,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'crawler.pipelines.CrawlerPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#     'crawler.pipelines.CrawlerPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
---
+++ crawler/crawler/spiders/tripadvisor_attraction.py
@@ -0,0 +1,47 @@
+import scrapy
+from crawler.items import CrawlerItem
+# from requests.models import PreparedRequest
+
+
+class AttractionSpider(scrapy.Spider):
+    name = "attraction"
+
+    # start_urls = ['']
+    base_url = "https://tripadvisor.com.my"
+    allowed_domains = ["tripadvisor.com"]
+
+    def start_requests(self):
+        urls = [
+            'https://www.tripadvisor.com.my/Attraction_Products-g293951-Malaysia.html',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, dont_filter=True, callback=self.parse_category)
+
+    def parse_category(self, response):
+        cat = response.xpath('//div[@class="filter_list_0"]')[0]
+        tour_categories = cat.xpath('./div/label/a/text()')
+        category_links = cat.xpath('./div/label/a/@href')
+        if cat.xpath('./div[@class="collapse hidden"]'):
+            category_links += cat.xpath(
+                './div[@class="collapse hidden"]/div/label/a/@href')
+            tour_categories += cat.xpath(
+                './div[@class="collapse hidden"]/div/label/a/text()')
+
+        category_links = [self.base_url + i for i in category_links.extract()]
+        tour_categories = ['_'.join(i.split(' ')[:-1]).lower()
+                           for i in tour_categories.extract()]
+        for i in range(len(tour_categories)):
+            yield scrapy.Request(url=category_links[i], meta={'category': tour_categories[i]}, dont_filter=True, callback=self.parse_attraction)
+
+    def parse_attraction(self, response):
+        for attr in response.xpath('//div[@data-slot]//div[@class="listing_title"]/a/@href'):
+            yield {
+                'attraction_url': attr.extract(),
+                'category': response.meta['category'],
+            }
+
+        next_page = response.xpath("//div[@class='unified pagination ']")
+        if next_page != [] and next_page.xpath("./span/@class").extract_first() != "nav next disabled":
+                url = next_page.xpath("./a/@href").extract_first()
+                # next_page.xpath("./a/@data-page-number").extract_first()
+                yield scrapy.Request(url=self.base_url + url, meta={'category': response.meta['category']}, dont_filter=True, callback=self.parse_attraction)
