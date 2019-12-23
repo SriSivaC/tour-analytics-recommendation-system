3d3e8c288c499b197f7093f7e177508c91408f14
liho98 <tanlh-wa16@student.tarc.edu.my>
2019-12-23 20:25:09 +0000

    optimize consumer

--- crawler/consumer.py
+++ crawler/consumer.py
@@ -8,7 +8,8 @@ import happybase
 from datetime import datetime
 from calendar import timegm
 
-KAFKA_TOPICS = ["theculturetrip", "tripadvisor_attraction"]
+KAFKA_TOPICS = ["theculturetrip", "tripad_location",
+                "tripad_activity", "tripad_review"]
 
 families = {
     'm': dict(max_versions=1),  # metadata
@@ -43,33 +44,50 @@ def utcnow_timestamp():
     return timegm(datetime.utcnow().timetuple())
 
 
-def kafka_to_hbase(kafka_topic):
-    consumer = KafkaConsumer(kafka_topic,
-                             bootstrap_servers=['localhost:9092'], consumer_timeout_ms=5000, auto_offset_reset='earliest', enable_auto_commit=False, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+def kafka_to_hbase(kafka_topic, key, column):
     hbase = HBaseBackend(happybase.Connection(
         'localhost', protocol='compact', transport='framed'))
 
-    for message in consumer:
-        # message value and key are raw bytes -- decode if necessary!
-        # e.g., for unicode: `message.value.decode('utf-8')`
-        # print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-        #                                      message.offset, message.key,
-        #                                      message.value))
+    consumer = KafkaConsumer(kafka_topic,
+                             bootstrap_servers=['localhost:9092'], consumer_timeout_ms=5000, auto_offset_reset='earliest', enable_auto_commit=False, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-        url = json.loads(json.loads(message.value))[
-            'props']['pageProps']['articleStoreState']['articleData']['data']['link']
+    for message in consumer:
+        url_id = json.loads(json.dumps(message.value))
+
+        for k in key:
+            url_id = url_id[k]
 
         if not hbase.table_exist(kafka_topic):
             hbase.create_table(kafka_topic, families)
 
         table = hbase.get_table(kafka_topic)
 
-        hbase.insert_data(table, url, {b'm:url': url})
-        hbase.insert_data(table, url, {b'm:created_at': str(utcnow_timestamp())})
-        hbase.insert_data(table, url, {b'c:content': message.value})
-        
+        hbase.insert_data(
+            table, str(url_id), {bytes(column[0], encoding='utf-8'): str(url_id)})
+        hbase.insert_data(
+            table, str(url_id), {b'm:created_at': str(utcnow_timestamp())})
+        hbase.insert_data(table, str(url_id), {b'c:content': json.dumps(message.value)})
+
     print("done.")
 
 
+# theculturetrip url
+# kafka_to_hbase(KAFKA_TOPICS[0], [
+#                "props, pageProps, articleStoreState, articleData, data, link"], ["m:url"])
+# tripadvisor location url
+# kafka_to_hbase(KAFKA_TOPICS[1], ["web_url"], ["m:url"])
+# tripadvisor activity id
+# kafka_to_hbase(KAFKA_TOPICS[2], [
+#                "productHeader", "activityId"], ["m:activityId"])
+# tripadvisor review's activity id
+kafka_to_hbase(KAFKA_TOPICS[3], [0, "data",
+                                 "locations", 0, "locationId"], ["m:locationId"])
 
-kafka_to_hbase(KAFKA_TOPICS[0])
+# message value and key are raw bytes -- decode if necessary!
+# e.g., for unicode: `message.value.decode('utf-8')`
+# print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                      message.offset, message.key,
+#                                      message.value))
+
+# with open('data.txt', 'w') as outfile:
+#     json.dump(review, outfile)
